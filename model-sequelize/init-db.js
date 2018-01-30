2f957624cd77f0e726e6ebb34c1002574e2445bd
witt <mymeek@163.com>
2018-01-30 21:22:58 +0800

    fix  init-db.js

--- model-sequelize/init-db.js
+++ model-sequelize/init-db.js
@@ -1,13 +1,2 @@
-require('babel-core/register')({
-    presets: ['stage-3']
-});
-
 const model = require('./model');
-models.sequelize.sync().then(() => {
-    console.log('sync done, db inited');
-    console.log('init db ok.');
-    process.exit(0);
-}).catch((e) => {
-    console.log(`failed:${e}`);
-    process.exit(0);
-});
+model.sync();
